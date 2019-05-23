59e7fd5801527a006731efad4c3d3bbe62080cb8
Randika Weerasooriya <randikapw@gmail.com>
2019-05-23 21:41:19 +0530

    Introduce currency exchange GET resource

---
+++ currency-exchange-service/src/main/java/com/randika/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,55 @@
+package com.randika.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+	private long id;
+	private String from,to;
+	private BigDecimal conversionMultiple;
+	
+	public ExchangeValue(String from, String to, long id, BigDecimal conversionMultiple) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.id = id;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	protected ExchangeValue() {
+		super();
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	
+	
+}

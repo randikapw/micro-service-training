3e904772de1fa6734d292fea3487d8809f8e5da2
Randika Weerasooriya <randikapw@gmail.com>
2019-05-24 11:13:27 +0530

    Modified proxy to call through Api gate instead of directly calling to currency-exchange

--- currency-conversion-service/src/main/java/com/randika/currencyconversionservice/CurrencyExchagneServiceProxy.java
+++ currency-conversion-service/src/main/java/com/randika/currencyconversionservice/CurrencyExchagneServiceProxy.java
@@ -5,11 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service")
-@RibbonClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server") //server name changed to zuul API manager name for rest requests.
+@RibbonClient(name="currency-exchange-service") //currency-exchange-service should be pointed ribbon as same as
+												// previous coz client side load balancing is happening for the
+												// servers registered to the name 'currency-exchange-service'
 public interface CurrencyExchagneServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyCovnersionBean retrieveExchangeValue
 		(@PathVariable String from,@PathVariable String to);
 }
